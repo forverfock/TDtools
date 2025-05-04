27b856bbe16df1b60fe2109481c850aeab2a4eb8
forverfock <forverfo@gmail.com>
2025-05-05 01:38:09 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,40 @@
+const CACHE_NAME = 'machining-calculator-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('machining-calc-v1').then(cache => {
-      return cache.addAll([
-        'index.html',
-        'manifest.json'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
